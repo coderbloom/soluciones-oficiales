db7d0dee7d7da2f1c25cce094cc9ee3c26fac6ee
Nancy Pacheco <nancy.pacheco@coderbloom.org>
2025-02-03 19:20:18 -0800

    Agregando la solución oficial de Conteo de Mariposas V1

---
+++ concursos/2025-01/Conteo-de-Mariposas-V1/solucion.py
@@ -0,0 +1,5 @@
+# Solución oficial de https://omegaup.com/course/ConcursosCoderBloom2025/assignment/Febrero-Intermedio/#problems/Conteo-de-Mariposas-V1
+N = int(input())
+m_i = map(int, input().split())
+
+print(sum(m_i))
