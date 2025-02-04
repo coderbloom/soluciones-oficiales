89a97e740734608c275f9bdf655f3bba9f6d15c3
Nancy Pacheco <nancy.pacheco@coderbloom.org>
2025-02-03 18:55:13 -0800

    Subiendo solución oficial de una-vida-migrante

---
+++ concursos/2025-01/una-vida-migrante/solucion.py
@@ -0,0 +1,4 @@
+# Solución oficial del problema https://omegaup.com/course/ConcursosCoderBloom2025/assignment/Concurso-Principiantes/#problems/Una-vida-migrante
+
+D, A = map(int, input().split())
+print(D * A)
