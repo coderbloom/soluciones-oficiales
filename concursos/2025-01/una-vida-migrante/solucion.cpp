fcd021e1b4f8dedab0f2be830de3dae5c7f88816
Nancy Pacheco <nancy.pacheco@coderbloom.org>
2025-02-03 18:56:33 -0800

    Create solucion.cpp
    Subiendo solución oficial de una-vida-migrante en C++

---
+++ concursos/2025-01/una-vida-migrante/solucion.cpp
@@ -0,0 +1,11 @@
+// Solución oficial de https://omegaup.com/course/ConcursosCoderBloom2025/assignment/Concurso-Principiantes/#problems/Una-vida-migrante
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int D, A;
+  cin >> D >> A;
+  cout << D * A << '\n';
+  return 0;
+}
