d56b026f43891cd00bcb764d3cd11764236bf766
Nancy Pacheco <nancy.pacheco@coderbloom.org>
2025-02-03 19:18:31 -0800

    Agregando la solución oficial de Conteo de Mariposas V0

---
+++ concursos/2025-01/Conteo-de-Mariposas-V0/solucion.py
@@ -0,0 +1,4 @@
+# Solución oficial de https://omegaup.com/course/ConcursosCoderBloom2025/assignment/Concurso-Principiantes/#problems/Conteo-de-Mariposas-V0
+
+N, M = map(int, input().split())
+print(N * M)
